181e1f5be800167e6603a5b0621a2b07ea45f64c
steph26260 <stephanie_seon@hotmail.com>
2026-03-26 11:28:28 +0100

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect, useCallback } from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
